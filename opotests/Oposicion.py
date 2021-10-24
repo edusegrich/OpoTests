023d314726ccd9e3a6e6b472bc7f3fa6af415d29
Alejandro Vega Jiménez <61701855+aleveji@users.noreply.github.com>
2021-10-24 20:03:54 +0200

    Añado Oposicion.py

---
+++ opotests/Oposicion.py
@@ -0,0 +1,20 @@
+class Oposicion:
+    """
+    Clase utilizada para representar una Oposicion
+
+    ...
+
+    Attributes
+    ----------
+    id : int
+        identificador de la oposicion
+    titulo : str
+        titulo de la oposicion
+    descripcion : str
+        descripcion  de la oposicion
+    """
+
+    def __init__(self, id, titulo, descripcion):
+        self.id = id
+        self.titulo = titulo
+        self.descripcion = descripcion
