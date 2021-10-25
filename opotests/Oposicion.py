b904ea073ac2cfbec2c7eeeb9241f0591f95c974
Alejandro Vega Jiménez <61701855+aleveji@users.noreply.github.com>
2021-10-25 13:06:38 +0200

    Actualizo Oposicion.py (Avanza #18)

--- opotests/Oposicion.py
+++ opotests/Oposicion.py
@@ -8,13 +8,16 @@ class Oposicion:
     ----------
     id : int
         identificador de la oposicion
+    idsTemas : list
+        lista que contiene los ids de los temas que componen la oposicion
     titulo : str
         titulo de la oposicion
     descripcion : str
         descripcion  de la oposicion
     """
 
-    def __init__(self, id, titulo, descripcion):
+    def __init__(self, id, idsTemas, titulo, descripcion):
         self.id = id
+        self.idsTemas = idsTemas
         self.titulo = titulo
         self.descripcion = descripcion
