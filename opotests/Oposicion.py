41b8deec1b1cf766344f49d9e969bfe4e90a179f
aleveji <alejandrovegajimenez@gmail.com>
2021-11-14 20:26:06 +0100

    Cambia atributos privados y getter titulo clase Oposicion #27
    Establece los atributos de la clase como privados, añade el atributo fecha
    y añade el getter del atributo titulo, además de una función para añadir
    un tema al temario

--- opotests/Oposicion.py
+++ opotests/Oposicion.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 class Oposicion:
     """
     Clase utilizada para representar una Oposicion
@@ -6,18 +8,24 @@ class Oposicion:
 
     Attributes
     ----------
-    id : int
-        identificador de la oposicion
-    idsTemas : list
-        lista que contiene los ids de los temas que componen la oposicion
     titulo : str
-        titulo de la oposicion
-    descripcion : str
-        descripcion  de la oposicion
+        titulo de la oposición que servirá como identificador
+    temas : list
+        lista que contiene los temas que componen la oposición
+    fecha : date
+        fecha en la que tendrá lugar el examen de la oposición
     """
 
-    def __init__(self, id, idsTemas, titulo, descripcion):
-        self.id = id
-        self.idsTemas = idsTemas
-        self.titulo = titulo
-        self.descripcion = descripcion
+    def __init__(self, titulo, temas, fecha):
+        self.__titulo = titulo
+        self.__temas = temas
+        self.__fecha = fecha
+    
+    @property
+    def titulo(self):
+        """Devuelve el titulo de la Oposición"""
+        return self.__titulo
+
+    def add_tema(self, tema):
+        """Añade un tema al temario de la Oposición"""
+        pass
