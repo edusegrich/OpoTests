c0c4395bec4abff4c45269b7dad290831039f8b5
Eduardo Segura Richart <edusr@correo.ugr.es>
2021-12-02 09:16:09 +0100

    Añade tareas instalacion a tasks.py (#29)
    Añadidas las siguientes tareas a tasks.py:
    - installpoetry: tarea que instala poetry
    - installdependencies: tarea que ejecuta el comando de poetry para instalar las dependencias

--- tasks.py
+++ tasks.py
@@ -1,6 +1,21 @@
 from invoke import task, run
 
 @task
+def installpoetry(c):
+    """
+    Tarea para instalar el gestor de dependencias Poetry
+    """
+    print("Instalando el gestor de dependencias...")
+    run("curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -")
+
+@task
+def installdependencies(c):
+    """
+    Tarea para instalar las dependencias del proyecto
+    """
+    run("poetry install")
+
+@task
 def syntax(c):
     """
     Tarea para comprobar la sintaxis de los ficheros fuente.
