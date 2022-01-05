f6bb63fd3c436de833e306abe5c1878e4adcd21c
Eduardo Segura Richart <edusr@correo.ugr.es>
2022-01-05 01:01:17 +0100

    Realiza correcciones en tests y añade extension de assertpy (#24)
    Cambios para corregir errores de los tests y añade extensión 'esta_entre_las_claves' para verificar si un numero esta entre las claves de un diccionario.

--- test/test_preguntas.py
+++ test/test_preguntas.py
@@ -1,5 +1,5 @@
 import pytest
-from assertpy import assert_that
+from assertpy import assert_that, add_extension
 
 from importar import *
 from opotests.pregunta import Pregunta
@@ -7,19 +7,22 @@ from opotests.dificultad import Dificultad
 
 @pytest.fixture
 def datos_pregunta():
-    datos = importar_pregunta('preguntas.txt')
+    datos = importar_pregunta('test/preguntas.txt')
     return datos
 
 @pytest.fixture
 def pregunta():
-    return Pregunta(datos_pregunta)
+    datos_pregunta = importar_pregunta('test/preguntas.txt')
+    return Pregunta(datos_pregunta[0], datos_pregunta[1], datos_pregunta[2], datos_pregunta[3], datos_pregunta[4], datos_pregunta[5])
 
 def test_estado_pregunta():
     """
     Test para comprobar que la pregunta tiene los atributos correctos
     """
     # Comprobamos que la pregunta no esta vacia
-    assert_that(pregunta).is_not_empty()
+    # assert_that(pregunta).is_not_empty()
+    datos = importar_pregunta('test/preguntas.txt')
+    pregunta = Pregunta(datos[0], datos[1], datos[2], datos[3], datos[4], datos[5])
 
     # Comprobamos que la pregunta tiene un id
     assert_that(pregunta.id).is_instance_of(int)
@@ -49,7 +52,7 @@ def test_respuesta_correcta(pregunta):
     """
     Test para comprobar que la pregunta tiene una respuesta correcta asignada entre las posibles
     """
-    assert_that(pregunta.resp_correcta).is_in(pregunta.respuestas.keys())
+    assert_that(pregunta.resp_correcta).esta_entre_las_claves(pregunta.respuestas)
 
 def test_dificultad(pregunta):
     """
@@ -57,3 +60,19 @@ def test_dificultad(pregunta):
     """
     assert_that(pregunta.dificultad).is_in(Dificultad.Alto, Dificultad.Medio, Dificultad.Bajo)
 
+def esta_entre_las_claves(self, diccionario):
+    """
+    Extension de assertpy para verificar si un valor esta entre las claves de un diccionario
+    """
+    esta = False
+    for key in diccionario.keys():
+        if self.val == key:
+            esta = True
+            break
+    if esta == False:
+        self.error(f'{self.val} no esta')
+
+    return self
+
+# Añadimos la extension
+add_extension(esta_entre_las_claves)
