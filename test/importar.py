8bc6f5f8b4f00b443cb61fa1c36feeef9b3c96ce
Eduardo Segura Richart <edusr@correo.ugr.es>
2022-01-05 01:03:38 +0100

    Añade cambios de tipo en atributos importados (#24)

--- test/importar.py
+++ test/importar.py
@@ -1,4 +1,4 @@
-import os
+from opotests.dificultad import Dificultad
 
 def importar_pregunta(ruta):
     """
@@ -6,7 +6,19 @@ def importar_pregunta(ruta):
     """
     with open(ruta, 'r') as fichero:
         pregunta = fichero.readline()
-        salida = pregunta.split(sep=',')
+        salida = pregunta.split(sep='|')
+
+        # Pasamos el 'id' a int
+        salida[0] = int(salida[0])
+
+        # Pasamos las 'respuestas' a diccionario
+        salida[3] = eval(salida[3])
+
+        # Pasamos la 'resp_correcta' a int
+        salida[4] = int(salida[4])
+
+        # Pasamos la 'dificultad' a un objeto de tipo Dificultad
+        salida[-1] = dificultad(salida[-1])
     
     return salida
 
@@ -19,7 +31,16 @@ def importar_preguntas(ruta):
         salida = []
 
         for p in preguntas:
-            pregunta = p.split(sep=',')
+            pregunta = p.split(sep='|')
             salida.append(pregunta)
     
     return salida
+
+def dificultad(str):
+    """
+    Funcion para pasar un string a objeto de tipo dificultad
+    """
+    dificultades = { 'Alto': Dificultad.Alto, 
+                     'Medio': Dificultad.Medio, 
+                     'Bajo': Dificultad.Bajo}
+    return dificultades[str]
