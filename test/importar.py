57b065cedd93aeabaf7f8e210da550fb3ed12821
Eduardo Segura Richart <edusr@correo.ugr.es>
2022-01-05 02:03:45 +0100

    Añade funcion para importar datos prueba de oposiciones para tests

--- test/importar.py
+++ test/importar.py
@@ -1,8 +1,14 @@
 from opotests.dificultad import Dificultad
+from opotests.grupo import Grupo
+from opotests.tema import Tema
+from datetime import date
 
 def importar_pregunta(ruta):
     """
-    Funcion para obtene una pregunta de un fichero .txt
+    Funcion para obtener una pregunta de un fichero .txt
+
+    Formato:
+    id|tema|enunciado|respuestas|respuesta correcta|dificultad
     """
     with open(ruta, 'r') as fichero:
         pregunta = fichero.readline()
@@ -22,6 +28,31 @@ def importar_pregunta(ruta):
     
     return salida
 
+def importar_oposicion(ruta):
+    """
+    Funcion para obtener una pregunta de un fichero .txt
+
+    Formato:
+    titulo|grupo|lista temas|fecha
+    """
+    with open(ruta, 'r') as fichero:
+        pregunta = fichero.readline()
+        salida = pregunta.split(sep='|')
+
+        # Pasamos el 'grupo' a un objeto de tipo Grupo
+        salida[1] = grupo(salida[1])
+
+        # Obtenemos los titulos de los temas y construimos una lista de objetos de tipo Tema
+        lista_temas = list(salida[2])
+        salida[2] = []
+        for titulo_tema in lista_temas:
+            salida[2].append(Tema(titulo_tema))
+
+        # Pasamos la fecha a un objeto de tipo Date
+        salida[3] = date(salida[3])
+    
+    return salida
+
 def importar_preguntas(ruta):
     """
     Funcion para obtener las preguntas de un fichero .txt (una pregunta en cada linea)
@@ -38,9 +69,21 @@ def importar_preguntas(ruta):
 
 def dificultad(str):
     """
-    Funcion para pasar un string a objeto de tipo dificultad
+    Funcion para pasar un string a objeto de tipo Dificultad
     """
     dificultades = { 'Alto': Dificultad.Alto, 
                      'Medio': Dificultad.Medio, 
                      'Bajo': Dificultad.Bajo}
     return dificultades[str]
+
+def grupo(str):
+    """
+    Funcion para pasar un string a objeto de tipo Grupo
+    """
+    grupos = { 'A1': Grupo.A1, 
+               'A2': Grupo.A2, 
+               'B': Grupo.B,
+               'C1': Grupo.C1, 
+               'C2': Grupo.C2, 
+               'E': Grupo.E}
+    return grupos[str]
